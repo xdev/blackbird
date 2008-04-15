82bd5bf43c55edfa876e2cff5b2fa20bc6e5bb7a
Joshua Rudd <joshuarudd@gmail.com>
2008-04-15 18:16:07 +0000

    [CHANGED] example config_custom.php now looks for config_local.php

--- example_config/config_custom.php
+++ example_config/config_custom.php
@@ -2,16 +2,30 @@
 
 /* $Id$ */
 
-// Set database values
+/* Set database values in config_local.php
 $GLOBALS['DATABASE'] = array(
 	'host' => '',
 	'user' => '',
 	'pass' => '',
 	'db'   => ''
 );
+*/
 
 // Set Site/Client name
 setConfig("CMS_CLIENT","Blackbird");
+// Set users table
+setConfig('CMS_USERS_TABLE','cms_users');
 
 // If you want to display an RSS feed in the homepage, set the RSS URL here
 // setConfig("CMS_NEWS_FEED",'http://url.com/for/rss/feed');
+
+// ini_set('display_errors',1);
+// error_reporting(E_ALL);
+
+// Get DB settings from local config_local.php file
+if (file_exists($_SERVER['DOCUMENT_ROOT'].'/cms_config/config_local.php')) {
+	include 'config_local.php';
+} else {
+	die('<h1>Site not configured.</h1>');
+}
+
