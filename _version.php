851c3c8e93bee7f6b409fdf76c1f479f21d110cc
Joshua Rudd <joshuarudd@gmail.com>
2008-04-20 05:55:31 +0000

    GENERAL TAGGING/BRANCHING
    [TAGGED] 1.0.14
    
    [CHANGED] _version.php now just has version number (not revision number)... and the trunk is called "trunk" - doesn't include a version number
    
    [CHANGED] BlackBird.class.php no longer displays revision number in CMS footer
    
    [FIXED] all tagged versions now properly include external bobolink revision that corresponds when blackbird was tagged (so old blackbird versions won't include newer versions of bobolink that may be incompatible)
    
    [DELETED] useless 1.0 branch

--- _version.php
+++ _version.php
@@ -2,6 +2,4 @@
 
 /* $Id$ */
 
-setConfig("BLACKBIRD_VERSION","1.0.14");
-setConfig("BLACKBIRD_REVISION",substr('$Revision$',11,-2));
-setConfig("REQUIRED_SCHEMA_VERSION","1.0.14");
+setConfig("BLACKBIRD_VERSION","trunk");
