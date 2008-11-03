4f42441fcbf2399047ebf117f1dc3f81a940e75f
Joshua Rudd <joshua@bricksf.com>
2008-11-03 15:16:21 -0800

    Add Brickhouse fix for route syntax to Blackbird's custom routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,7 +1,7 @@
 <?php
 
-$routes[] = array('uri' => "/^\/(?<controller>table)\/(?<action>browse)\/(?<table>[a-z_0-9]+)$/");
-$routes[] = array('uri' => "/^\/(?<controller>record)\/(?<action>edit)\/(?<table>[a-z_0-9]+)\/(?<id>[a-zA-Z_0-9]+)$/");
-$routes[] = array('uri' => "/^\/(?<controller>record)\/(?<action>add)\/(?<table>[a-z_0-9]+)$/");
-$routes[] = array('uri' => "/^\/(?<controller>user)\/(?<action>profile)\/(?<user>[0-9]+)$/");
+$routes[] = array('uri' => "/^\/(?P<controller>table)\/(?P<action>browse)\/(?P<table>[a-z_0-9]+)$/");
+$routes[] = array('uri' => "/^\/(?P<controller>record)\/(?P<action>edit)\/(?P<table>[a-z_0-9]+)\/(?P<id>[a-zA-Z_0-9]+)$/");
+$routes[] = array('uri' => "/^\/(?P<controller>record)\/(?P<action>add)\/(?P<table>[a-z_0-9]+)$/");
+$routes[] = array('uri' => "/^\/(?P<controller>user)\/(?P<action>profile)\/(?P<user>[0-9]+)$/");
 $routes[] = array('uri' => "/^\/(about)$/",'controller'=>'page','action'=>'about');
