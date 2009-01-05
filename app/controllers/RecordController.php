af18ec5d0e82f638520675e50cd594419a0ea0ab
Joshua Rudd <joshua@bricksf.com>
2009-01-05 13:40:02 -0800

    Fix RecordController Process to properly store field values (again)

--- app/controllers/RecordController.php
+++ app/controllers/RecordController.php
@@ -248,7 +248,7 @@ class RecordController extends _Controller
 						case "module":
 
 
-						break;
+							break;
 
 						case "plugin":
 							$options['table'] = $this->table;
@@ -259,7 +259,7 @@ class RecordController extends _Controller
 							
 							_ControllerFront::pluginColumnEdit($_name_space . $column['name'],$value, $options);
 							$col_ready = true;
-						break;
+							break;
 
 						case "position":
 
@@ -288,7 +288,7 @@ class RecordController extends _Controller
 
 							Forms::selectDefault($_name_space . $column['name'],$value, $options);
 							$col_ready = true;
-						break;
+							break;
 						
 						case "slug":
 							
@@ -302,12 +302,12 @@ class RecordController extends _Controller
 							';
 							$col_ready = true;
 							
-						break;
+							break;
 
 
 						case "disabled":
 							$col_ready = true;
-						break;
+							break;
 
 
 						default :
@@ -327,7 +327,7 @@ class RecordController extends _Controller
 							
 							Forms::$module($_name_space . $column['name'],$value, $options);
 							$col_ready = true;
-						break;
+							break;
 
 					}
 
@@ -341,25 +341,25 @@ class RecordController extends _Controller
 
 				switch(true){		
 
-					case ($col_type == 'datetime') :
+					case ($col_type == 'datetime' || $col_type == 'timestamp') :
 						Forms::selectDateTime($_name_space . $column['name'],$value, $options );
-					break;
+						break;
 
 					case ($col_type == 'date') :
 						Forms::selectDate($_name_space . $column['name'],$value, $options );
-					break;
+						break;
 
 					case ($col_type == 'time') :
 						Forms::selectTime($_name_space . $column['name'],$value, $options );
-					break;
+						break;
 
 					case (substr($col_type,0,4) == "text") :
 						Forms::textarea($_name_space . $column['name'],$value, $options );
-					break;
+						break;
 
 					default :
 						Forms::text($_name_space . $column['name'],$value, $options );
-					break;
+						break;
 				}
 			
 			}	
@@ -377,7 +377,6 @@ class RecordController extends _Controller
 	public function Process()
 	{
 		$this->layout_view = null;
-		
 		
 		//server side validation
 		$this->_name_space = $_POST['name_space'] . '_';
@@ -401,9 +400,8 @@ class RecordController extends _Controller
 		
 		//set up error handler here
 		$this->errorData = array();		
-						
+		
 		foreach($q_cols as $col){
-			
 			
 			$col_type = strtolower($col['Type']);
 			$col_ready = false;
@@ -418,246 +416,242 @@ class RecordController extends _Controller
 			//get anything from the blackbird_cols
 			if($q_sql = $this->db->query("SELECT * FROM ".BLACKBIRD_TABLE_PREFIX."cols WHERE column_name = '$col[Field]' AND process_module != '' ORDER BY table_name,process_mode")){
 				$q_c = array_merge($q_c,$q_sql);
-			}					
-						
+			}
+			
+			// Check process mode
 			$q_col = Utils::checkArray($q_c,array('table_name'=>$this->table,'process_mode'=>$this->query_action));
 			if(!$q_col){
 				$q_col = Utils::checkArray($q_c,array('table_name'=>$this->table,'process_mode'=>''));
 			}
-			
 			if(!$q_col){
 				$q_col = Utils::checkArray($q_c,array('table_name'=>'*','process_mode'=>$this->query_action));
 				if(!$q_col){
 					$q_col = Utils::checkArray($q_c,array('table_name'=>'*','process_mode'=>''));
 				}
 			}
-									
-			if($q_col){
 			
-				$module = $q_col['process_module'];
+			$module = $q_col ? $q_col['process_module'] : '';
+			
+			switch (true):
 				
-				switch(true){
+				case $module == 'plugin' || $module == 'file':
+					$options = array();
+					$options['mode'] = $this->query_action;
+					$options['name_space'] = $this->_name_space;
+					$options['db'] = AdaptorMysql::getInstance();
 					
-					case $module == 'plugin' || $module == 'file':
-						$options = array();
-						$options['mode'] = $this->query_action;
-						$options['name_space'] = $this->_name_space;
-						$options['db'] = AdaptorMysql::getInstance();
+					if($this->query_action == "update"){
+						$options['id'] = $this->id;
+					}
+					if($this->query_action == "insert"){
+						$options['id'] = $this->db->getInsertId($this->table);
+					}
+					$options['col_name'] = $col['Field'];
+					$options['table'] = $this->table;
+					
+					if(isset($_REQUEST[$this->_name_space . $col['Field']])){
+						$value = $_REQUEST[$this->_name_space . $col['Field']];
+					}else{
+						$value = '';
+					}
+					
+					if(strlen($q_col['process_config']) > 1){
+						$options = array_merge($options,_ControllerFront::parseConfig($q_col['process_config']));
+					}
+					
+					if($module == 'plugin'){
 						
-						if($this->query_action == "update"){
-							$options['id'] = $this->id;
-						}
-						if($this->query_action == "insert"){
-							$options['id'] = $this->db->getInsertId($this->table);
-						}
-						$options['col_name'] = $col['Field'];
-						$options['table'] = $this->table;
+						$t = _ControllerFront::pluginColumnProcess($this->_name_space . $col['Field'],$value, $options);
 						
-						if(isset($_REQUEST[$this->_name_space . $col['Field']])){
-							$value = $_REQUEST[$this->_name_space . $col['Field']];
+						if(isset($t['error'])){
+							$this->errorData[] = array('field'=>$col['Field'],'error'=>$t['error']);	
 						}else{
-							$value = '';
+							if(is_array($t)){
+								$row_data[] = $t;
+							}
 						}
+					}
+					
+					if($module == 'file'){
 						
-						if(strlen($q_col['process_config']) > 1){
-							$options = array_merge($options,_ControllerFront::parseConfig($q_col['process_config']));
-						}
+						$name = $this->_name_space . $col['Field'];
+						$upload = true;
 						
-						if($module == 'plugin'){
-							
-							$t = _ControllerFront::pluginColumnProcess($this->_name_space . $col['Field'],$value, $options);
-							
-							if(isset($t['error'])){
-								$this->errorData[] = array('field'=>$col['Field'],'error'=>$t['error']);	
-							}else{
-								if(is_array($t)){
-									$row_data[] = $t;
+						if(isset($options['file_validator']) && is_uploaded_file($_FILES[$name]['tmp_name'])){
+							$t = Utils::validateFile($_FILES[$name],$options['file_validator']);
+							if($t === true){
+								
+							}else if(is_array($t)){
+								$r = '<ul>';
+								foreach($t as $row){
+									$r .= '<li>'.$row.'</li>';
 								}
+								$r .= '</ul>';
+								$this->errorData[] = array('field'=>$col['Field'],'error'=>$r);
+								$upload = false;
 							}
 						}
 						
-						if($module == 'file'){
-							
-							$name = $this->_name_space . $col['Field'];
-							$upload = true;
-							
-							if(isset($options['file_validator']) && is_uploaded_file($_FILES[$name]['tmp_name'])){
-								$t = Utils::validateFile($_FILES[$name],$options['file_validator']);
-								if($t === true){
-									
-								}else if(is_array($t)){
-									$r = '<ul>';
-									foreach($t as $row){
-										$r .= '<li>'.$row.'</li>';
+						//if so.. do upload
+						if($upload === true){
+							if($value = Utils::uploadFile($name,$value,$options)) {
+								
+								$row_data[] = array(
+									'field'=>$options['col_name'],
+									'value'=>$value
+								);
+								
+								if(isset($options['thumbnails'])){
+									foreach($options['thumbnails'] as $thumb){
+										$src = WEB_ROOT . 'files/'.$options['table'].'/'.$options['col_name'].'/'.$value;
+										$targ = WEB_ROOT . 'files/'.$options['table'].'/'.$thumb['output_directory'].'/image_'.$options['id'].'.jpg';
+										Utils::createThumb($src,$targ,$thumb['height'],$thumb['width'],array('quality'=>$thumb['quality'],'mode'=>$thumb['mode']));
 									}
-									$r .= '</ul>';
-									$this->errorData[] = array('field'=>$col['Field'],'error'=>$r);
-									$upload = false;
 								}
-							}
-							
-							//if so.. do upload
-							if($upload === true){
-								if($value = Utils::uploadFile($name,$value,$options)) {
-									
-									$row_data[] = array(
-										'field'=>$options['col_name'],
-										'value'=>$value
-									);
-									
-									if(isset($options['thumbnails'])){
-										foreach($options['thumbnails'] as $thumb){
-											$src = WEB_ROOT . 'files/'.$options['table'].'/'.$options['col_name'].'/'.$value;
-											$targ = WEB_ROOT . 'files/'.$options['table'].'/'.$thumb['output_directory'].'/image_'.$options['id'].'.jpg';
-											Utils::createThumb($src,$targ,$thumb['height'],$thumb['width'],array('quality'=>$thumb['quality'],'mode'=>$thumb['mode']));
-										}
-									}
 
-								}elseif (isset($_POST[$name.'_delete']) && $_POST[$name.'_delete']) {
-									$row_data[] = array(
-										'field'=>$options['col_name'],
-										'value'=>''
-									);
-								}
+							}elseif (isset($_POST[$name.'_delete']) && $_POST[$name.'_delete']) {
+								$row_data[] = array(
+									'field'=>$options['col_name'],
+									'value'=>''
+								);
 							}
 						}
-						
-						$col_ready = true;
-					break;
+					}
 					
-					case $module == 'position':
-						//if we are a position column
-						$where = '';
-						
-						if(strlen($q_col['process_config']) > 1){
-							$config = _ControllerFront::parseConfig($q_col['process_config']);
-						}else if(isset($config)){
-							unset($config);
-						}
-						
-						$value = $_REQUEST[$this->_name_space . $col['Field']];
-						
-						if($this->query_action == "update"){
-							
-							//check for constraints from config
-							if(isset($config['col_constraint'])){
-								//try to find in row_data
-								$foundrow = false;
-								foreach($row_data as $temprow){
-									if($temprow['field'] == $config['col_constraint']){
-										$foundrow = true;
-										$where = "WHERE `".$config['col_constraint']."` = '".$temprow['value']."' ";
-									}
-								}
-								if(!$foundrow){
-									//check for the $_REQUEST
-									$where = "WHERE `".$config['col_constraint']."` = '".$_REQUEST[$this->_name_space . $config['col_constraint']]."' ";
-								}
-							}
-							
-							_ControllerFront::sortPosition($this->table,"SELECT id FROM `$this->table` $where ORDER BY $col[Field]",$this->id,$value,$col['Field']);
-						}
-						if($this->query_action == "insert"){
-							//check for constraints from config
-							if(isset($config)){
-								$where = "WHERE `".$config['col_constraint']."` = '".$_REQUEST[$this->_name_space . $config['col_constraint']]."' ";
-							}
-							$q_pos = $this->db->queryRow("SELECT max($col[Field]) AS position FROM `$this->table` $where");
-							$row_data[] = array("field"=>$col['Field'],"value"=>($q_pos['position'] + 1));
-						}
-						$col_ready = true;
+					$col_ready = true;
 					break;
+				
+				case $module == 'position':
+					//if we are a position column
+					$where = '';
 					
-					case $module == 'slug':
+					if(strlen($q_col['process_config']) > 1){
+						$config = _ControllerFront::parseConfig($q_col['process_config']);
+					}else if(isset($config)){
+						unset($config);
+					}
+					
+					$value = $_REQUEST[$this->_name_space . $col['Field']];
+					
+					if($this->query_action == "update"){
 						
-						function checkSlug($slug,$options)
-						{
-							if ($slug != '*' && $q = $options['db']->query("
-								SELECT ".$options['col_name']."
-								FROM ".$options['table']."
-								WHERE id != '".$options['id']."'
-									AND ".$options['col_name']." = '".$slug."'".$options['where']."
-							")) {
-								if (is_numeric($i = substr($slug,strrpos($slug,'_')+1))) $slug = substr($slug,0,strrpos($slug,'_')+1).($i+1);
-								else $slug .= '_1';
-								return checkSlug($slug,$options);
-							} else {
-								return $slug;
-							}
-						}
-						
-						if(strlen($q_col['process_config']) > 1){
-							$config = _ControllerFront::parseConfig($q_col['process_config']);
-						}else if(isset($config)){
-							unset($config);
-						}
-						
-						$value = $_REQUEST[$this->_name_space . $col['Field']];
-						if ($this->query_action == 'insert') {
-							$this->id = mysql_insert_id();
-							//$q_pos = $this->db->queryRow("SELECT max($col[Field]) FROM `$this->table` $where");
-							//$this->id = $q_pos[0] + 1;
-						}
 						//check for constraints from config
-						$where = "";
 						if(isset($config['col_constraint'])){
 							//try to find in row_data
+							$foundrow = false;
 							foreach($row_data as $temprow){
 								if($temprow['field'] == $config['col_constraint']){
-									$where = " AND `".$temprow['field']."` = '".$temprow['value']."' ";
+									$foundrow = true;
+									$where = "WHERE `".$config['col_constraint']."` = '".$temprow['value']."' ";
 								}
 							}
+							if(!$foundrow){
+								//check for the $_REQUEST
+								$where = "WHERE `".$config['col_constraint']."` = '".$_REQUEST[$this->_name_space . $config['col_constraint']]."' ";
+							}
 						}
-						$value = checkSlug($value,array(
-							'col_name' => $col['Field'],
-							'table' => $this->table,
-							'id' => $this->id,
-							'where' => $where,
-							'db' => $this->db
-						));
-						/*if ($value != '*' && $q = $this->db->query("
-							SELECT ".$col['Field']."
-							FROM ".$this->table."
-							WHERE id != '".$this->id."'
-								AND ".$col['Field']." = '".$value."'".$where."
-						")) {
-							if (is_numeric($i = substr($value,strrpos($value,'_')+1))) $value = substr($value,0,strrpos($value,'_')+1).($i+1);
-							else $value = $value.'_1';
-						}*/
-						$row_data[] = array("field"=>$col['Field'],"value"=>$value);
-						$col_ready = true;
-					break;
-					
-					case $module == 'timestamp':
-						$row_data[] = array("field"=>$col['Field'],"value"=>(($col['Field'] == 'created' && $_REQUEST[$this->_name_space . $col['Field']]) ? $_REQUEST[$this->_name_space . $col['Field']] : Utils::now()));
-						$col_ready = true;
-					break;
-					
-					case $col_type == "datetime" || $col_type == "timestamp":
-						$row_data[] = array("field"=>$col['Field'],"value"=>Utils::assembleDateTime($col['Field'],$this->_name_space));
-						$col_ready = true;
-					break;
-					
-					case $col_type == "date":
-						$row_data[] = array("field"=>$col['Field'],"value"=>Utils::assembleDate($col['Field'],$this->_name_space));
-						$col_ready = true;
-					break;
-					
-					case $col_type == "time":
-						$row_data[] = array("field"=>$col['Field'],"value"=>Utils::assembleTime($col['Field'],$this->_name_space));
-						$col_ready = true;
-					break;
-					
-					default:
-						//if we are a generic column
-						if(isset($_REQUEST[$this->_name_space . $col['Field']])){
-							$row_data[] = array("field"=>$col['Field'],"value"=>$_REQUEST[$this->_name_space . $col['Field']]);
+						
+						_ControllerFront::sortPosition($this->table,"SELECT id FROM `$this->table` $where ORDER BY $col[Field]",$this->id,$value,$col['Field']);
+					}
+					if($this->query_action == "insert"){
+						//check for constraints from config
+						if(isset($config)){
+							$where = "WHERE `".$config['col_constraint']."` = '".$_REQUEST[$this->_name_space . $config['col_constraint']]."' ";
 						}
+						$q_pos = $this->db->queryRow("SELECT max($col[Field]) AS position FROM `$this->table` $where");
+						$row_data[] = array("field"=>$col['Field'],"value"=>($q_pos['position'] + 1));
+					}
+					$col_ready = true;
 					break;
-					
-				}
 				
-			}
+				case $module == 'slug':
+					
+					function checkSlug($slug,$options)
+					{
+						if ($slug != '*' && $q = $options['db']->query("
+							SELECT ".$options['col_name']."
+							FROM ".$options['table']."
+							WHERE id != '".$options['id']."'
+								AND ".$options['col_name']." = '".$slug."'".$options['where']."
+						")) {
+							if (is_numeric($i = substr($slug,strrpos($slug,'_')+1))) $slug = substr($slug,0,strrpos($slug,'_')+1).($i+1);
+							else $slug .= '_1';
+							return checkSlug($slug,$options);
+						} else {
+							return $slug;
+						}
+					}
+					
+					if(strlen($q_col['process_config']) > 1){
+						$config = _ControllerFront::parseConfig($q_col['process_config']);
+					}else if(isset($config)){
+						unset($config);
+					}
+					
+					$value = $_REQUEST[$this->_name_space . $col['Field']];
+					if ($this->query_action == 'insert') {
+						$this->id = mysql_insert_id();
+						//$q_pos = $this->db->queryRow("SELECT max($col[Field]) FROM `$this->table` $where");
+						//$this->id = $q_pos[0] + 1;
+					}
+					//check for constraints from config
+					$where = "";
+					if(isset($config['col_constraint'])){
+						//try to find in row_data
+						foreach($row_data as $temprow){
+							if($temprow['field'] == $config['col_constraint']){
+								$where = " AND `".$temprow['field']."` = '".$temprow['value']."' ";
+							}
+						}
+					}
+					$value = checkSlug($value,array(
+						'col_name' => $col['Field'],
+						'table' => $this->table,
+						'id' => $this->id,
+						'where' => $where,
+						'db' => $this->db
+					));
+					/*if ($value != '*' && $q = $this->db->query("
+						SELECT ".$col['Field']."
+						FROM ".$this->table."
+						WHERE id != '".$this->id."'
+							AND ".$col['Field']." = '".$value."'".$where."
+					")) {
+						if (is_numeric($i = substr($value,strrpos($value,'_')+1))) $value = substr($value,0,strrpos($value,'_')+1).($i+1);
+						else $value = $value.'_1';
+					}*/
+					$row_data[] = array("field"=>$col['Field'],"value"=>$value);
+					$col_ready = true;
+					break;
+				
+				case $module == 'timestamp':
+					$row_data[] = array("field"=>$col['Field'],"value"=>(($col['Field'] == 'created' && $_REQUEST[$this->_name_space . $col['Field']]) ? $_REQUEST[$this->_name_space . $col['Field']] : Utils::now()));
+					$col_ready = true;
+					break;
+				
+				case $col_type == "datetime" || $col_type == "timestamp":
+					$row_data[] = array("field"=>$col['Field'],"value"=>Utils::assembleDateTime($col['Field'],$this->_name_space));
+					$col_ready = true;
+					break;
+				
+				case $col_type == "date":
+					$row_data[] = array("field"=>$col['Field'],"value"=>Utils::assembleDate($col['Field'],$this->_name_space));
+					$col_ready = true;
+					break;
+				
+				case $col_type == "time":
+					$row_data[] = array("field"=>$col['Field'],"value"=>Utils::assembleTime($col['Field'],$this->_name_space));
+					$col_ready = true;
+					break;
+				
+				default:
+					//if we are a generic column
+					if(isset($_REQUEST[$this->_name_space . $col['Field']])){
+						$row_data[] = array("field"=>$col['Field'],"value"=>$_REQUEST[$this->_name_space . $col['Field']]);
+					}
+					break;
+				
+			endswitch;
 			
 		}
 				
